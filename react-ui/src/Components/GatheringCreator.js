f0c6d77f90bfedd63d02b9ea3d2ac5f47d302dbe
Jason Redcedar <jredcedar@Jasons-MacBook-Air.local>
2021-10-06 16:56:58 -0700

    more inputs for noobs, actually sorta working

--- react-ui/src/Components/GatheringCreator.js
+++ react-ui/src/Components/GatheringCreator.js
@@ -1,66 +1,87 @@
-import React from "react";
+import React, {useState, useEffect} from 'react';
 import { gql, useMutation } from '@apollo/client';
 
 const CREATE_GATHERING = gql`
-  mutation CreateGathering($newGathering: CreateGatheringInput!) {
-    createGathering(input: $newGathering) {
-      gathering {
-        name
-        description
-      }
+mutation CreateGathering($name: String!, $description: String) {
+  createGathering(
+    input: { 
+      gathering: { 
+        name: $name 
+        description: $description 
+      } 
+    }
+  ) {
+    gathering {
+      name
+      description
     }
   }
-`;
+}`;
 
 
-const GatheringCreator = ({gathering}) => {
+const GatheringCreator = () => {
 
-    let input;
     const [createGathering, { data, loading, error }] = useMutation(CREATE_GATHERING);
+    const [isSaveDisabled, setIsSaveDisabled] = useState(true);
+
+    const [values, setValues] = useState({
+        name: '',
+        description: ''
+      });
+
+
+    const setFormStateValues = target => {
+      const values = {[target.name]: target.value};
+      //setValues(prevState => ({
+      //    ...prevState, ...values
+      //}));
+    }
     
+    const handleInputChange = e => {
+      e.preventDefault();
+      setFormStateValues(e.target)
+    }
+
+    const handleSubmit = e => {
+      e.preventDefault();
+      const gatheringName = document.getElementById("gatheringName").value;
+
+      createGathering({variables: 
+                    {
+                      name: gatheringName,
+                      description: 'test'
+                    }
+                  });
+
+    }
+
     if (loading) return 'Submitting...';
     if (error) return `Submission error! ${error.message}`;
 
     return (
         <div>
-          <form
-            onSubmit={e => {
-              e.preventDefault();
-              createGathering({ variables: 
-                { 
-                  newGathering: {
-                    gathering: {
-                        name: input.value,
-                        description: "just this for now"
-                     }
-                  }
-             } });          
-              input.value = '';
-            }}
-          >
+          <form onSubmit={handleSubmit}>
             <div>
+            
                 <label for="gatheringName">Name:</label>
                 <input
                 name="gatheringName"
                 id="gatheringName"
-                ref={node => {
-                    input = node;
-                }}
+                //value={values.name}
+                onChange={handleInputChange} 
                 />
             </div>
             <div>
                 <label for="gatheringDescription">Description:</label>
                 <input
                 name="gatheringDescription"
-                id="gatheringDescription"
-                ref={node => {
-                    input = node;
-                }}
+                //value={values.description}
+                onChange={handleInputChange} 
                 />
             </div>
 
 
-            <button type="submit">Create Gathering</button>
+            <button name="save" type='submit' aria-label='save-gathering'>Save</button>
           </form>
         </div>
       );
